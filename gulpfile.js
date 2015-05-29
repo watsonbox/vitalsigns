848a2e1b192cf77f19fa8131c54131a3919cb262
Howard Wilson <howard@watsonbox.net>
2015-05-29 15:41:52 +0200

    Don’t actually need Reactify, since Babelify does the job

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,19 @@
 var gulp = require('gulp');
 
 var browserify = require('browserify');
-var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 var babelify = require('babelify');
 
 gulp.task('js', function(){
   browserify('./public/javascripts/src/app.jsx')
     .transform(babelify)
-    .transform(reactify)
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('public/javascripts/build/'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch("public/javascripts/src/**/*.jsx", ["js"])
+  gulp.watch("public/javascripts/src/**/*.jsx", ["js"])
 })
 
 gulp.task('default', ['js', 'watch']);
