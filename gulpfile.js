dd034c457994e08cd02d0bbd5f018ad3c63e2276
Howard Wilson <howard@watsonbox.net>
2015-05-30 13:44:31 +0200

    Add SASS support

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var babelify = require('babelify');
 var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
 
 gulp.task('js', function(){
   browserify('./public/javascripts/src/app.jsx')
@@ -13,8 +14,15 @@ gulp.task('js', function(){
     .pipe(gulp.dest('public/javascripts/build/'));
 });
 
+gulp.task('styles', function() {
+  gulp.src('public/stylesheets/style.scss')
+    .pipe(sass({onError: function(e) { console.log(e); } }))
+    .pipe(gulp.dest('public/stylesheets/build/'));
+});
+
 gulp.task('watch', function() {
   gulp.watch("public/javascripts/src/**/*.jsx", ["js", browserSync.reload])
+  gulp.watch("public/stylesheets/**/*.scss", ["styles", browserSync.reload]);
 })
 
 gulp.task('browsersync', ['js'], function () {
